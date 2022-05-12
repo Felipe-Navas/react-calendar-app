9449cf642303497fa9bd0cacab28c2d06009bbd6
Felipe Navas <felipenavas.itec@gmail.com>
2022-05-12 20:45:57 -0300

    Adding moment, react-big-calendar, some styles for the calendar and the calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 import { Navbar } from '../ui/Navbar'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = momentLocalizer(moment)
+
+const myEventsList = [
+  {
+    title: 'All Day Event very long title',
+    allDay: true,
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#FFCE56',
+  },
+]
 
 export const CalendarScreen = () => {
   return (
-    <div>
+    <div className="calendar-screen">
       <Navbar />
-      <h1>Calendar Screen</h1>
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+      />
     </div>
   )
 }
